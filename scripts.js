26b45fac31fd2fa218f977a09ba7ae0c2d6ad27d
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-02-18 21:32:40 +0100

    Added photos, styles middle sections, calculated days to go

--- scripts.js
+++ scripts.js
@@ -1,2 +1,38 @@
 // Init onePageScroll
 onePageScroll(".main");
+
+// Init Parallax
+document.addEventListener("DOMContentLoaded", function (event) {
+    var containers = [
+        document.querySelector('.we-are-getting-married'),
+        document.querySelector('.vanessa'),
+        document.querySelector('.francesco'),
+        document.querySelector('.save-the-date'),
+        document.querySelector('.join-us'),
+        document.querySelector('.our-photos'),
+        document.querySelector('.days-to-go'),
+        document.querySelector('.honeymoon')
+    ];
+
+    containers.map(function (container) {
+        return new Parallax(container)
+    });
+
+    // work out days to go
+    function daysBetween(one, another) {
+        another = new Date('06-21-2018');
+        var isDue = one.getTime() >= another.getTime();
+        if (isDue) {
+            return "OVER!";
+        }
+        return Math.round(Math.abs((+ one) - (+ another)) / 8.64e7);
+    }
+
+    function setDaysToGoText() {
+        var counter = document.querySelector('.days-to-go__counter');
+        var daysToGo = daysBetween(new Date());
+        counter.innerHTML = daysToGo;
+    }
+
+    setDaysToGoText();
+});
